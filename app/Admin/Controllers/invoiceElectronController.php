921c0415101d2f188adc18cb9ab87c2897a5fd32
liangweibin <liangweibin@maka.im>
2018-03-29 23:49:38 +0800

    [ADD] invoiceElectronController

---
+++ app/Admin/Controllers/invoiceElectronController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Admin\Controllers;
+
+class invoiceElectronController extends BaseController
+{
+    //电子发票列表
+    public function invoiceElectron()
+    {
+        
+    }
+
+    //电子发票审核页面
+    public function invoiceElectronAudit()
+    {
+
+    }
+
+    //电子发票发送邮件
+    public function invoiceElectronSend()
+    {
+
+    }
+}
